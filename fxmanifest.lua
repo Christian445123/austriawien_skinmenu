7fb93c6a00fc6d41e12323a147010d83c9d08e36
Christian445123 <christian@frato.at>
2026-05-09 14:52:41 +0200

    Update 1.0.5

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 name        'austriawien_skinmenu'
 description 'AustriaWien – Skin-Menü für ESX'
-version     '1.0.4'
+version     '1.0.5'
 author      'GamingDevelopment'
 
 dependencies {
